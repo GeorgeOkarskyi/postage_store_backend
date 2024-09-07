5779945d7dd006c617d4056c04e7d0de750e944f
Heorhii Okarskyi <heorhiiokarskyi@Heorhiis-MacBook-Pro.local>
2024-09-07 17:35:41 -0600

    postgresSQL&microORM: added checkout route

--- src/cart/cart.router.ts
+++ src/cart/cart.router.ts
@@ -4,9 +4,9 @@ import { validatRequestBody } from '../middlewares/validation.middleware'
 
 export const cartRouter = Router();
 
-const { requestUserCart, updateUserCart, emptyUserCart } = CartController;
+const { requestUserCart, updateUserCart, emptyUserCart, chackoutUserCart } = CartController;
 
 cartRouter.get('/', requestUserCart);
 cartRouter.put('/', validatRequestBody, updateUserCart);
 cartRouter.delete('/', emptyUserCart);
-// cartRouter.post('/checkout', validatRequestBody,chackoutUserCart);
+cartRouter.post('/checkout', validatRequestBody, chackoutUserCart);
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -35,11 +35,11 @@ class CartService {
         return updateUserCartResponce;
     }
 
-    // async chackoutUserCart(userId: string, { payment, delivery, comments, status }: { payment: Payment, delivery: Delivery, comments: string, status: ORDER_STATUS }): Promise<OrderEntity>{
-    //     const checkoutUserCartResponse = await this.cartDAL.chackoutUserCart(userId, payment, delivery, comments, status);
+    async chackoutUserCart(userId: string, { payment, delivery, comments, status }: { payment: Payment, delivery: Delivery, comments: string, status: ORDER_STATUS }): Promise<OrderEntity>{
+        const checkoutUserCartResponse = await this.cartDAL.chackoutUserCart(userId, payment, delivery, comments, status);
         
-    //     return checkoutUserCartResponse;
-    // }
+        return checkoutUserCartResponse;
+    }
 }
 
 const productDAL = new ProductDAL();
